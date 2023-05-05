c9faf340b25b147f7408bc380f19c5f522a16d76
antoivanov <antoivanov@korusconsulting.ru>
2023-05-05 15:19:05 +0300

    feat: add initial configuration, add packages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import eslint from "vite-plugin-eslint"
+import dynamicImport from 'vite-plugin-dynamic-import'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()]
+  plugins: [react(), eslint(), dynamicImport()],
+  // esbuild: {
+  //   jsxInject: `import React from 'react'`,
+  // },
+  resolve: {
+    alias: {
+      'src': path.resolve(__dirname, './src'),
+    }
+  }
 })
